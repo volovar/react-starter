0224597420ac1dd089a01be876f390b7e6a7fbaf
volovar <michaelvolovar@gmail.com>
2019-07-18 22:28:15 -0400

    [STRTR | 0006] Updated Packages
    Updated package versions, replaced `emotion` with `@emotion/core`.

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { css } from "emotion";
+import { css } from "@emotion/core";
 
 const appStyles = css`
     background: lightblue;
@@ -9,7 +9,7 @@ const appStyles = css`
 
 class App extends Component {
     render() {
-        return <div className={appStyles}>Hello React Starter</div>;
+        return <div css={appStyles}>Hello React Starter</div>;
     }
 }
 
